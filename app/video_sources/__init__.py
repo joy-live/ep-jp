5d336c333ec07d38efba16fa878baff89c3ad575
joy-live <joy@grows.live>
2025-04-28 03:59:02 +0530

    Delete app directory

--- app/video_sources/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import VideoSourceResponse, BaseVideoSource
-from .sahaniji_source import SahanijiVideoSource
-from .kingnish_source import KingnishVideoSource
-from .bytedance_source import ByteDanceVideoSource
-
-__all__ = ['VideoSourceResponse', 'BaseVideoSource', 'SahanijiVideoSource', 'KingnishVideoSource', 'ByteDanceVideoSource'] 
